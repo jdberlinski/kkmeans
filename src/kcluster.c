5d697e759867ab5fb1652913907fd2b3e1c06a55
Josh Berlinski <josh.berlinski@gmail.com>
2020-06-16 15:46:44 -0500

    kmeans++

--- src/kcluster.c
+++ src/kcluster.c
@@ -72,15 +72,15 @@ void kcluster(double *x,
   /* original cluster assignment here */
 
   /* randomize the clusters */
-  for (j = 0; j < n; j++)
-  {
-    current_clust = rand_dunif(k);
+  /* for (j = 0; j < n; j++) */
+  /* { */
+  /*   current_clust = rand_dunif(k); */
 
-    n_k[current_clust]++;
+  /*   n_k[current_clust]++; */
 
-    ic1[j] = current_clust;
-    ic2[j] = (current_clust + 1) % k;
-  }
+  /*   ic1[j] = current_clust; */
+  /*   ic2[j] = (current_clust + 1) % k; */
+  /* } */
 
 
   for (i = 0; i < n; i++)
@@ -100,46 +100,68 @@ void kcluster(double *x,
    *      = k(x, x) - 2k(x, c) + k(c, c)
    */
 
-  /* int init_clust = rand_dunif(n); */
-  /* int* init_centers = (int *) S_alloc(k, sizeof(int)); */
-  /* init_centers[0] = init_clust; */
-  /* double* dist_kpp = (double *) S_alloc(, sizeof(int)); */
-  /* double* dist_kpp2 = (double *) S_alloc(, sizeof(int)); */
-  /* int current_center = 0; */
+  /* new cluster assignment using kmeans++ */
 
-  /* double t_dist = big; */
+  int init_clust = rand_dunif(n);
+  int* init_centers = (int *) S_alloc(k, sizeof(int));
+  init_centers[0] = init_clust;
+  double* dist_kpp = (double *) S_alloc(n, sizeof(int));
+  double* dist_kpp2 = (double *) S_alloc(n, sizeof(int));
+  int current_center = 0;
 
-  /* for (i = 0; i < n; i++) */
-  /* { */
-  /*   /1* ic1 is initially 0 *1/ */
-  /*   current_clust = ic1[i]; */
-  /*   current_center = init_centers[current_clust]; */
-  /*   dist_kpp = kernel_matrix[get_index(i, i, n)] - */
-  /*     2 * kernel_matrix[get_index(i, current_center, n)] + */
-  /*     kernel_matrix[get_index(current_clust, current_clust, n)]; */
+  double t_dist = big;
 
-  /*   dist_kpp[i] *= dist_kpp[i]; */
+  for (i = 0; i < n; i++)
+  {
+    /* ic1 is initially 0 */
+    current_clust = ic1[i];
+    current_center = init_centers[current_clust];
+    dist_kpp[i] = kernel_matrix[get_index(i, i, n)] -
+      2 * kernel_matrix[get_index(i, current_center, n)] +
+      kernel_matrix[get_index(current_center, current_center, n)];
 
-  /*   dist_kpp2[i] = big; */
-  /*   n_k[0]++; */
-  /* } */
+    dist_kpp[i] *= dist_kpp[i];
 
-  /* for (j = 0; j < k; j++) */
-  /* { */
-  /*   GetRNGState(); */
-  /*   rmultinom(1, dist_kpp, n, &init_centers[j]); */
-  /*   PutRNGState(); */
+    dist_kpp2[i] = big;
+    n_k[0]++;
+  }
 
-  /*   for (i = 0; i < n; i++) */
-  /*   { */
-  /*     current_clust = ic1[i]; */
-  /*     t_dist = kernel_matrix[get_index(i, i, n)] - */
-  /*       2 * kernel_matrix[get_index(i, current_center, n)] + */
-  /*       kernel_matrix[get_index(current_clust, current_clust, n)]; */
+  for (j = 1; j < k; j++)
+  {
+    GetRNGState();
+    rmultinom(1, dist_kpp, n, &init_centers[j]);
+    PutRNGState();
 
+    current_clust = init_centers[j];
 
-  /*   } */
-  /* } */
+    for (i = 0; i < n; i++)
+    {
+      // t_dist is the distance from the point to its current center
+      // but I already have that, so I need to compute the distance from each
+      // point to the newly chosen center.
+      // The distance from the point to its current center is going to be stored
+      // in the dist_kpp matrix
+      t_dist = kernel_matrix[get_index(i, i, n)] -
+        2 * kernel_matrix[get_index(i, current_center, n)] +
+        kernel_matrix[get_index(current_clust, current_clust, n)];
+
+      t_dist *= t_dist;
+
+      if (t_dist < dist_kpp[i])
+      {
+        ic2[i] = ic1[i];
+        ic1[i] = current_clust;
+        dist_kpp2[i] = dist_kpp[i];
+        dist_kpp[i] = t_dist;
+      }
+      else if (t_dist < dist_kpp2[i])
+      {
+        ic2[i] = current_clust;
+        dist_kpp2[i] = t_dist;
+      }
+
+    }
+  }
 
 
 
